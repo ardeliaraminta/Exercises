b95a82cb2dfec535bb6114bcbd8bd2564f694ef9
ardel <71622783+ardeliaraminta@users.noreply.github.com>
2020-11-17 22:48:52 +0700

    Update car_assignment.py

--- car_assignment.py
+++ car_assignment.py
@@ -8,33 +8,34 @@ class Car:
         self.fuel_usage = fuel_usage 
 
 #maximum distance of a car ride  1 km 
-def distance(self) -> float:
-    per_km = 100/ self.fuel_usage * self.tank_capacity
-    return per_km
+    def distance(self) -> float:
+        per_km = 100/ self.fuel_usage * self.tank_capacity
+        return per_km
 
 #maximum duration
-def maximum_duration(self) -> float:
-    return distance()/self.speed * 60
+    def maximum_duration(self) -> float:
+        return distance()/self.speed * 60
 
 #car comparison 
-def is_better_than(self, other_car):
-    if self.distance() > other_car.distance() and self.maximum_duration() > other_car.maximum_duration:
-        return "yes"
-    elif self.distance > other_car.distance() or self.maximum_duration() > other_car.maximum_duration:
-        return "maybe"
-    else:
-        return "no"
+    def is_better_than(self, other_car):
+        if self.distance() > other_car.distance() and self.maximum_duration() > other_car.maximum_duration:
+            return "yes"
+        elif self.distance > other_car.distance() or self.maximum_duration() > other_car.maximum_duration:
+            return "maybe"
+        else:
+            return "no"
 
-def compare_cars(self, other_company):
-    for c in other_company.cars:
-        if self.is_better_than(c)== "yes":
-            self.counter += 1
-    print(f" Our car, {self.model} is better than {self.counter} out of {(other_company.car)} of all the cars in the company.")
+        def compare_cars(self, other_company):
+            for c in other_company.cars:
+                if self.is_better_than(c)== "yes":
+                    self.counter += 1
+        print(f" Our car, {self.model} is better than {self.counter} out of {(other_company.car)} of all the cars in the company.")
 
 class Company:
-    def __init__(self, name, car):
-        self.name = name
-        self.car = car 
+        
+        def __init__(self, name, car):
+            self.name = name
+            self.car = car 
 
     
 car1 = Car("Model S", 50, 37, 8.5)
